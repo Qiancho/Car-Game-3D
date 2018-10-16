0f369ba0eb8daaf741c9a8d47d4d0d91437ed0b4
Unknown <qiany@email.arizona.edu>
2018-10-16 13:00:31 -0700

    Trees

--- Car Game/Assets/Scripts/Camera.cs
+++ Car Game/Assets/Scripts/Camera.cs
@@ -8,13 +8,13 @@ public class Camera : MonoBehaviour {
     public float CarX;
     public float CarY;
     public float CarZ;
-	
-	// Update is called once per frame
-	void Update () {
-		CarX = Car.transform.eulerAngles.x;
+    
+    // Update is called once per frame
+    void Update () {
+        CarX = Car.transform.eulerAngles.x;
         CarY = Car.transform.eulerAngles.y;
         CarZ = Car.transform.eulerAngles.z;
 
-        transform.eulerAngles = new Vector3 (CarX - CarX, CarY, CarZ - CarZ);
-	}
+        transform.eulerAngles = new Vector3 (0f, CarY, 0f);
+    }
 }
