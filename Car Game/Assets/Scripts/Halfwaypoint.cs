50cd9962c6e1eeb6336c27d6bee1248f85386bef
Unknown <qiany@email.arizona.edu>
2018-10-23 14:32:55 -0700

    Halfway and finish points

---
+++ Car Game/Assets/Scripts/Halfwaypoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Halfwaypoint : MonoBehaviour {
+
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    void OnTriggerEnter () {
+        LapCompleteTrig.SetActive (true);
+        HalfLapTrig.SetActive (false);
+    }
+}
