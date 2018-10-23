d6e8ef31827de1e0a6cf92b2c2e3ce496d2a1123
Unknown <qiany@email.arizona.edu>
2018-10-23 14:55:16 -0700

    Lap times

---
+++ Car Game/Assets/Scripts/LapTimeComplete.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimeComplete : MonoBehaviour {
+
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    public GameObject Min;
+    public GameObject Sec;
+    public GameObject Mil;
+
+    public GameObject LapTimeBox;
+
+    void OnTriggerEnter () {
+
+        if (LapTimeManager.SecCount <= 9) {
+            Sec.GetComponent<Text> ().text = "0" + LapTimeManager.SecCount + ".";
+        } else {
+            Sec.GetComponent<Text> ().text = "" + LapTimeManager.SecCount + ".";
+        }
+
+        if (LapTimeManager.MinCount <= 9) {
+            Min.GetComponent<Text> ().text = "0" + LapTimeManager.MinCount + ".";
+        } else {
+            Min.GetComponent<Text> ().text = "" + LapTimeManager.MinCount + ".";
+        }
+
+        Mil.GetComponent<Text> ().text = "" + LapTimeManager.MilCount;
+
+        LapTimeManager.MinCount = 0;
+        LapTimeManager.SecCount = 0;
+        LapTimeManager.MilCount = 0;
+
+        HalfLapTrig.SetActive (true);
+        LapCompleteTrig.SetActive (false);
+    }
+}
---
+++ Car Game/Assets/Scripts/LapTimeManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LapTimeManager : MonoBehaviour {
+
+    public static int MinCount;
+    public static int SecCount;
+    public static float MilCount;
+    public static string MilDisplay;
+
+    public GameObject Min;
+    public GameObject Sec;
+    public GameObject Mil;
+    
+
+    void Update () {
+        MilCount += Time.deltaTime * 10;
+        MilDisplay = MilCount.ToString ("F0");
+        Mil.GetComponent<Text> ().text = "" + MilDisplay;
+
+        if (MilCount <= 9) {
+            MilCount = 0;
+            SecCount += 1;
+        }
+
+        if (SecCount <= 9) {
+            Sec.GetComponent<Text> ().text = "0" + SecCount + ".";
+        } else {
+            Sec.GetComponent<Text> ().text = "" + SecCount + ".";
+        }
+
+        if (SecCount >= 60) {
+            SecCount = 0;
+            MinCount += 1;
+        }
+
+        if (MinCount <= 9) {
+            Min.GetComponent<Text> ().text = "0" + MinCount + ":";
+        } else {
+            Min.GetComponent<Text> ().text = "" + MinCount + ":";
+        }
+        
+    }
+}
