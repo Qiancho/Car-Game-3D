2d2a6ac830e839f103d27f5268b5f78db75552c8
Unknown <qiany@email.arizona.edu>
2018-10-29 21:02:51 -0700

    Main Menu

---
+++ Car Game/Assets/Scripts/LoadLevels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevels : MonoBehaviour {
+    public string leveltoload;
+    public bool exit = true;
+    public void loadlevel(){
+        SceneManager.LoadScene(leveltoload);
+    }
+
+    // public void exitgame(){
+    //     if (Input.GetKeyDown(KeyCode.Escape)) {
+    //     Application.Quit();
+    // }
+}
